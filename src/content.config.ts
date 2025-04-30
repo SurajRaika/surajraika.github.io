5a215bf96aeb93f6b426f1ae386ba538104f11dc
Suraj Raika <surajraika5sr@gmail.com>
2025-04-30 20:37:16 +0530

    v

--- src/content.config.ts
+++ src/content.config.ts
@@ -26,21 +26,21 @@ const blog = defineCollection({
   }),
 });
 
-// const projects = defineCollection({
-//   loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/projects" }),
-//   schema: z.object({
-//     title: z.string(),
-//     description: z.string(),
-//     date: z.coerce.date(),
-//     draft: z.boolean().optional(),
-//     demoURL: z.string().optional(),
-//     repoURL: z.string().optional(),
-//   }),
-// });
+const projects = defineCollection({
+  loader: glob({ pattern: '**/*.{md,mdx}', base: "./src/content/projects" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    date: z.coerce.date(),
+    draft: z.boolean().optional(),
+    demoURL: z.string().optional(),
+    repoURL: z.string().optional(),
+  }),
+});
 
 
 
 export const collections = { blog, 
-  // projects 
+  projects 
 
 };
